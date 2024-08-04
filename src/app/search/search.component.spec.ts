f6593b122ef6d81b4e6e0062799630525fa69b57
Scott.Tucker <scott.tucker@eblusolutions.com>
2024-08-04 14:25:26 -0400

    changing a unit test

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,18 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import { MoviesService } from '../services/movies.service';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
+  let mockMovieService: MoviesService;
   let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchComponent]
-    })
-    .compileComponents();
-    
+      imports: [SearchComponent],
+      providers: [{ provide: MoviesService }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SearchComponent);
+    mockMovieService = TestBed.inject(MoviesService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
